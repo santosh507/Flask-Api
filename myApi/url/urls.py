2302b1087d089d4520ebb1c1393e6e69872a0aa3
Santosh <santoheg@cisco.com>
2019-03-11 01:44:10 +0530

    Practice Api corrections

--- myApi/url/urls.py
+++ myApi/url/urls.py
@@ -1,5 +1,5 @@
 from myApi.url import myApi
-from myApi.implementation.api_test import test_api,test_api_1
+from myApi.implementation.api_test import test_api,test_api_clone
 
 myApi.add_url_rule('/testApi',"test api",test_api,methods=['GET'])
-myApi.add_url_rule('/testApi1',"test api 1",test_api_1,methods=['GET'])
+myApi.add_url_rule('/testApiClone',"test api clone",test_api_clone,methods=['GET'])
