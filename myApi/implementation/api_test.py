3143dbb207d59f131b26f9bb616cdcbd1f3e2fd7
Santosh <santoheg@cisco.com>
2019-03-11 01:24:46 +0530

    Adding new testApi1

--- myApi/implementation/api_test.py
+++ myApi/implementation/api_test.py
@@ -2,3 +2,6 @@ from flask import jsonify
 
 def test_api():
     return jsonify({"Message":"Hello World"})
+
+def test_api_1():
+    return jsonify({"Message":"Hello World"})
--- myApi/url/urls.py
+++ myApi/url/urls.py
@@ -1,4 +1,5 @@
 from myApi.url import myApi
-from myApi.implementation.api_test import test_api
+from myApi.implementation.api_test import test_api,test_api_1
 
-myApi.add_url_rule('/TestApi',"test api",test_api,methods=['GET'])
+myApi.add_url_rule('/testApi',"test api",test_api,methods=['GET'])
+myApi.add_url_rule('/testApi1',"test api 1",test_api_1,methods=['GET'])
