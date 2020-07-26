253a83469bc640c40dafce36c2ae40501184750f
Santosh Hegde <santoheg@cisco.com>
2020-07-26 22:49:37 +0530

    specified routes inside create_app

--- wsgi/__init__.py
+++ wsgi/__init__.py
@@ -1,10 +1,10 @@
 from flask import Flask
 from wsgi.logger import Logger
-from wsgi.myApi.url import myApi, urls
 
 
 def create_app():
     app = Flask(__name__)
     app.debug = True
+    from wsgi.myApi.url import myApi, urls
     app.register_blueprint(myApi, url_prefix='/api')
     return app
